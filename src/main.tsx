7cd853b31f4ae367d78a9fcac285a5e5e818c923
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2025-01-04 23:00:28 -0400

    Add react-router & routes & layout & views

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1 className=" bg-red-700">HOLA MUNDO</h1>
+    <RouterProvider router={router} />{" "}
   </StrictMode>
 );
---
+++ src/router.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Products from "./views/Products";
+import NewProduct from "./views/NewProduct";
+import EditProduct from "./views/EditProduct";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Products />,
+      },
+      {
+        path: "productos/nuevo",
+        element: <NewProduct />,
+      },
+      {
+        path: "productos/:id/editar",
+        element: <EditProduct />,
+      },
+    ],
+  },
+]);
